3dc475daa113af23be6e12fa079e34e26f2207a4
BrandonMiyazaki <119912546+BrandonMiyazaki@users.noreply.github.com>
2026-02-03 17:10:51 -0800

    Add files via upload

---
+++ MDAV-Troubleshooting.ps1
@@ -0,0 +1,11 @@
+﻿New-MpPerformanceRecording -RecordTo recording.etl
+
+Get-MpPerformanceReport -Path C:\Temp\recording.etl -TopProcesses 3
+
+Get-MpPerformanceReport -Path C:\Temp\recording.etl -TopScans 3
+
+Get-MpPerformanceReport -Path C:\Temp\recording.etl -TopPaths 3
+
+Get-MpPerformanceReport -Path C:\Temp\recording.etl -TopProcesses 5 -TopFilesPerProcess 5
+
+Get-MpPerformanceReport -Path C:\Temp\recording.etl -Overview
